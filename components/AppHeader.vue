bc514c32bf98fcf3d1fc9396c68d537a82fd5686
Yevhenii Shalko <eugene.shalko.00@gmail.com>
2024-02-20 17:22:15 +0200

    Add index page and header

---
+++ components/AppHeader.vue
@@ -0,0 +1,25 @@
+<template>
+  <header class="flex justify-between items-center py-4">
+    <nuxt-link to="/" class="text-2xl font-bold">Finance</nuxt-link>
+    <nav>
+      <nuxt-link to="/" class="mr-4">Home</nuxt-link>
+      <nuxt-link to="/about">About</nuxt-link>
+    </nav>
+    <template>
+      <UAvatar
+          size="sm"
+          src="https://avatars.githubusercontent.com/u/739984?v=4"
+          alt="Avatar"
+      />
+    </template>
+
+  </header>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,6 @@
 <template>
-  <div class="container mx-auto max-w-4xl">
+  <div class="container mx-auto max-w-4xl mt-5">
+    <app-header />
     <main class="my-10">
       <slot />
     </main>
